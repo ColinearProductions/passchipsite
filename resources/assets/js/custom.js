ecafdbdcde9dc59b42bc646bc4a606431c9dced7
RazvanConic <razvan.becheru@conicdesign.ro>
2017-11-21 00:49:12 +0200

    Hotjar

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -53,13 +53,15 @@ $(document).on('ready', function () {
             alert("Incorrect Captcha");
             $(loader).hide();
         } else {
+            $(loader).hide();
+            showModal();
             $.ajax({ // create an AJAX call...
                 data: $(this).serialize(), // get the form data
                 type: $(this).attr('method'), // GET or POST
                 url: $(this).attr('action'), // the file to call
                 success: function (response) { // on success..
                     $(loader).hide();
-                    showModal();
+                    //showModal();
                 },
                 error: function (jqXHR, err) {
                     $(loader).hide();
@@ -82,3 +84,58 @@ function showModal(){
 $(modal).on('hidden.bs.modal', function () {
     location.reload();
 });
+
+
+
+
+
+
+var analytics_ID = "UA-103088784-2";
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', analytics_ID, 'auto');
+ga('send', 'pageview');
+
+
+
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:696704,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+
+
+
+
+$("a.download").on("click",function(){
+
+    ga('send','event','File','Download',this.href)
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
