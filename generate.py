64904c4866b026b239691f2022c80fa9759dde37
Razvan B <becheru.razvan@gmail.com>
2022-11-27 15:15:56 +0200

    chore: update contact form info

--- generate.py
+++ generate.py
@@ -14,7 +14,7 @@ def read_data(data_folder_path):
 
     for d in data_files:
         name = d.replace(".json", "")
-        with open(data_folder_path + os.sep + d) as json_file:
+        with open(data_folder_path + os.sep + d, 'r', encoding='utf-8', errors='ignore') as json_file:
             data = json.load(json_file)
         all_data[name] = data
 
@@ -25,18 +25,16 @@ def parse_mako_file(input_file_path, output_file_path, mylookup, data):
     print(input_file_path)
     mytemplate = mylookup.get_template("/" + input_file_path)
 
-    input_file = open(output_file_path, "r")
-    output = str(mytemplate.render(data=data))
+    input_file = open(output_file_path, "rb")
+    output = mytemplate.render(data=data)
     input_file.close()
-
-    new_contents = output.replace('\n', '')
-    output_file = open(output_file_path, 'w')
-    output_file.write(new_contents)
+    output_file = open(output_file_path, 'wb')
+    output_file.write(output)
     output_file.close()
 
 
 def run(src_folder, dst_folder, ignored_folders, trasnalted_data):
-    if os.path.isdir(dst_folder): #if destination file exists, remove it
+    if os.path.isdir(dst_folder):  # if destination file exists, remove it
         shutil.rmtree(dst_folder)
 
     shutil.copytree(src_folder, dst_folder)
@@ -55,9 +53,6 @@ def run(src_folder, dst_folder, ignored_folders, trasnalted_data):
                                 mylookup, trasnalted_data)
 
 
-
-
-
 with open('generate-options.json') as data_file:
     config_data = json.load(data_file)
 
@@ -69,10 +64,8 @@ languages = config_data['languages']
 
 
 for language in languages:
-    language_destination_folder = destination_folder + os.sep + language #todo remove comment
-    language_data = read_data(config_data['dataSourceFolder'] + os.sep + language)
+    language_destination_folder = destination_folder + \
+        os.sep + language  # todo remove comment
+    language_data = read_data(
+        config_data['dataSourceFolder'] + os.sep + language)
     run(source_folder, language_destination_folder, ignored_folders, language_data)
-
-
-
-
